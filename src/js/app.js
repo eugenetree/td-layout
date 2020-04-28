cc8bb6827707d3b6812976c4f3b2be82551ea6be
oziged <oziged@gmail.com>
2020-04-28 05:12:40 +0300

    static layout done

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,21 @@
 import '../scss/app.scss';
+import axios from 'axios';
 
-// Your JS Code goes here
+axios.get('./features.json')
+  .then(({data}) => renderSection(data))
+
+function renderSection(array) {
+  const nodeList = document.querySelectorAll('.feature-item')
+  if (array.length != nodeList.length) {
+    console.log('sorry, something goes wrong 😞')
+    return
+  }
+
+  nodeList.forEach((nodeItem, index) => {
+    const data = array[index]
+    const [img, title, desc] = nodeItem.querySelectorAll('.feature-item__img img, .feature-item__title, .feature-item__desc')
+    img.src = data.img
+    title.textContent = data.title
+    desc.textContent = data.description
+  })
+}
